09c453361d5cb2e59ae1b0f50c2a19ce491e37c4
Peter Bourgon <peter@bourgon.org>
2011-12-20 11:16:15 +0100

    Attempt to fix Linux/g++ 4.3 issues

--- hashmap.hh
+++ hashmap.hh
@@ -1,8 +1,18 @@
 #ifndef HASHMAP_HH_
 #define HASHMAP_HH_
 
-#include <ext/hash_map>
 #include <string>
+
+#if __GNUG__ >= 4 // __GNUG__ == (__GNUC__ && __cplusplus)
+# if __GNUC_MINOR__ == 2
+#include <ext/hash_map>
+# elif __GNUC_MINOR__ > 2
+#include <unordered_map>
+# endif
+#else
+// Lower versions are (probably) supportable, but I don't have a way to test
+#error g++ version 4.2 or higher is required
+#endif
 
 namespace __gnu_cxx
 {
